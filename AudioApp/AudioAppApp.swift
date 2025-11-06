d8c62177b613c5d5be3c2f429323a59c23486ae7
Omar Mendivil <omendivil8@gmail.com>
2025-11-06 11:24:02 -0800

    Initial Commit

---
+++ AudioApp/AudioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioAppApp.swift
+//  AudioApp
+//
+//  Created by Omar Mendivil on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
