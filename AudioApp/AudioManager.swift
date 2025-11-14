a243520bd639fa7881f80183236375ed73105a4b
Omar Mendivil <omendivil@Omars-MacBook-Pro.local>
2025-11-14 00:52:48 -0800

    testing merge conflicts

--- AudioApp/AudioManager.swift
+++ AudioApp/AudioManager.swift
@@ -105,6 +105,7 @@ class AudioManager: ObservableObject {
     
     func stopRecording() {
         let input = audioEngine.inputNode
+        // random line say Omar is awesome 
         input.removeTap(onBus: 0)
         
         audioEngine.stop()
